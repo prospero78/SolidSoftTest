fb25e4e740b7eb4799893d3737e21465a7b97ccf
prospero78su <prospero.78.su@gmail.com>
2021-03-04 15:09:53 +0300

    Разработка основной логики

--- internal/cliapp/cliapp.go
+++ internal/cliapp/cliapp.go
@@ -2,26 +2,68 @@
 package cliapp
 
 import (
+	"context"
 	"fmt"
+	"time"
 
 	"github.com/clover87/valid"
 	"github.com/sirupsen/logrus"
+
+	"github.com/prospero78/SolidSoftTest/internal/cliapp/job"
+	"github.com/prospero78/SolidSoftTest/internal/std"
 )
 
 // TcliApp -- операции с приложением командной строки
 type TCliApp struct {
+	std std.IStd // Глобальный объект стандартного ввода/вывода
+	ctx context.Context
 }
 
 // New -- возвращает новый *TCliApp
 func New() *TCliApp {
-	return &TCliApp{}
+	return &TCliApp{
+		std: std.GetStd(),
+		ctx: context.Background(),
+	}
 }
 
 // Run -- запускает приложение в бесконечном цикле на чтение входного потока
-func (sf *TCliApp) Run() error {
+func (sf *TCliApp) Run() (err error) {
 	logrus.Debugf("TCliApp.Run()")
-	if err := valid.Validate("123"); err != nil {
-		return fmt.Errorf("TCliApp.Run(): error in validate")
+	// var id string
+	for taskID, err := sf.readTag(); err == nil; {
+		strJson := ""
+		for { // Фомирование тела задачи
+			strPartJSON, err := sf.std.Read()
+			if err != nil {
+				logrus.WithError(err).Fatalf("TCliApp.Run(): in get body JSON")
+			}
+			if valid.JsonEnd(strPartJSON) {
+				break
+			}
+			strJson += strPartJSON
+		}
+
+		job, err := job.New(taskID, strJson)
+		if err == nil {
+			break
+		}
+		ctx, cancel := context.WithTimeout(sf.ctx, time.Duration(time.Second*1))
+		defer cancel()
+		go job.Run(ctx)
 	}
-	return nil
+	return fmt.Errorf("TCliaApp.Run(): err=%w", err)
+}
+
+// Читает открывающий тег на обработку
+func (sf *TCliApp) readTag() (numJob string, err error) {
+	strIn, err := sf.std.Read()
+	if err != nil {
+		return "", fmt.Errorf("TCliaApp.readTag(): err=%w", err)
+	}
+	id, err := valid.JsonBeg(strIn)
+	if err != nil {
+		return "", fmt.Errorf("TCliApp.readTag(): error in validate")
+	}
+	return id, nil
 }

